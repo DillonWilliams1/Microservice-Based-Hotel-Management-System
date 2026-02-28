52e20703ae5e89064dd7d7dbfcbe7dc8931aaa71
Dillon Williams <dillonwilliams200161@gmail.com>
2026-02-28 16:24:53 +0530

    implement reservation module with CRUD operations

--- backend/reservationManagementService/src/main/java/com/nsbm/group03/reservationManagementService/controller/ReservationController.java
+++ backend/reservationManagementService/src/main/java/com/nsbm/group03/reservationManagementService/controller/ReservationController.java
@@ -1,12 +1,37 @@
 package com.nsbm.group03.reservationManagementService.controller;
 
+import com.nsbm.group03.reservationManagementService.dto.ReservationDTO;
+import com.nsbm.group03.reservationManagementService.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/reservations")
+@CrossOrigin
 public class ReservationController {
 
+    @Autowired
+    private ReservationService reservationService;
 
+    @PostMapping
+    public ReservationDTO saveReservation(@RequestBody ReservationDTO dto) {
+        return reservationService.saveReservation(dto);
+    }
+
+    @GetMapping
+    public List<ReservationDTO> getAll() {
+        return reservationService.getAllReservations();
+    }
+
+    @GetMapping("/guest/{guestId}")
+    public List<ReservationDTO> getByGuest(@PathVariable Long guestId) {
+        return reservationService.getReservationsByGuest(guestId);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Long id) {
+        return reservationService.deleteReservation(id);
+    }
 }
--- backend/reservationManagementService/src/main/java/com/nsbm/group03/reservationManagementService/service/ReservationService.java
+++ backend/reservationManagementService/src/main/java/com/nsbm/group03/reservationManagementService/service/ReservationService.java
@@ -1,5 +1,62 @@
 package com.nsbm.group03.reservationManagementService.service;
 
+import com.nsbm.group03.reservationManagementService.dto.ReservationDTO;
+import com.nsbm.group03.reservationManagementService.entity.Guest;
+import com.nsbm.group03.reservationManagementService.entity.Reservation;
+import com.nsbm.group03.reservationManagementService.repository.GuestRepository;
+import com.nsbm.group03.reservationManagementService.repository.ReservationRepository;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
 public class ReservationService {
 
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    private GuestRepository guestRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    // Save Reservation
+    public ReservationDTO saveReservation(ReservationDTO reservationDTO) {
+
+        Guest guest = guestRepository.findById(reservationDTO.getGuestId())
+                .orElseThrow(() -> new RuntimeException("Guest not found"));
+
+        Reservation reservation = modelMapper.map(reservationDTO, Reservation.class);
+        reservation.setGuest(guest);
+        reservation.setCreatedAt(LocalDateTime.now());
+
+        reservationRepository.save(reservation);
+
+        return modelMapper.map(reservation, ReservationDTO.class);
+    }
+
+    // Get All Reservations
+    public List<ReservationDTO> getAllReservations() {
+        List<Reservation> list = reservationRepository.findAll();
+        return modelMapper.map(list, new TypeToken<List<ReservationDTO>>() {}.getType());
+    }
+
+    //  Get Reservations By Guest
+    public List<ReservationDTO> getReservationsByGuest(Long guestId) {
+        List<Reservation> list = reservationRepository.findByGuest_GuestId(guestId);
+        return modelMapper.map(list, new TypeToken<List<ReservationDTO>>() {}.getType());
+    }
+
+    // Delete Reservation
+    public boolean deleteReservation(Long id) {
+        reservationRepository.deleteById(id);
+        return true;
+    }
 }
